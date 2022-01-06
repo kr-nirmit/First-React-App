5c6e92cb6951360351ef3c48344fef32a6f12d40
kr-nirmit <nirmit.t@knackroot.com>
2022-01-06 10:55:03 +0530

    Add welcome.js Components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/welcome';
 
 function App() {
   const name = "Nirmit";
@@ -14,19 +15,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World {name}</p>
-        <p>{fullName(user)}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
     </div>
   );
 }
