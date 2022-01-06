20be54662c8657bc09830e13bb83e2138977a5b6
kr-nirmit <nirmit.t@knackroot.com>
2022-01-06 14:42:04 +0530

    Add Message.js file and create State

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import Welcome from './components/welcome';
 import Hello from './components/Hello';
 import Greet from './components/Greet';
+import Message from './components/Message';
 
 function App() {
   return (
     <div className="App">
-      <Welcome />
+      {/* <Welcome />
       <Greet />
       <Hello fname="Nirmit" sname="Thummar">
         <p>This is a children props</p>
         <p>This is second line</p>
       </Hello>
       <Hello fname="Sanket" sname="Thummar" />
-      <Hello fname="Meet" sname="Dabhi" />
+      <Hello fname="Meet" sname="Dabhi" /> */}
+      <Message />
     </div>
   );
 }
