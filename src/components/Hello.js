1af87c04ae61301b9d863333dcf0edad49ae0dd2
kr-nirmit <nirmit.t@knackroot.com>
2022-01-06 15:34:06 +0530

    Class Destructuring

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,23 @@
 import react, {Component} from "react";
 
+class Hello extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello {this.props.fname} {this.props.sname}...</h1>
+            </div>
+        ) 
+            
+    }
+}
+
+// Class Destructuring
 // class Hello extends Component{
 //     render(){
+//         const{fname,sname} = this.props
 //         return(
 //             <div>
-//                 <h1>Hello {this.props.fname} {this.props.sname}...</h1>
+//                 <h1>Hello {fname} {sname}...</h1>
 //             </div>
 //         ) 
             
@@ -20,6 +33,7 @@ import react, {Component} from "react";
 //     )
 // }
 
+// Element Destructuring
 // const Hello = ({fname, sname}) => {
 //     return(
 //         <div>
@@ -28,14 +42,15 @@ import react, {Component} from "react";
 //     )
 // }
 
-const Hello = props => {
-    const {fname , sname} = props
-    return(
-        <div>
-            <h1>Hello {fname} {sname}...</h1>
-        </div>
-    )
-}
+// Function Destructuring
+// const Hello = props => {
+//     const {fname , sname} = props
+//     return(
+//         <div>
+//             <h1>Hello {fname} {sname}...</h1>
+//         </div>
+//     )
+// }
     
 
 export default Hello
